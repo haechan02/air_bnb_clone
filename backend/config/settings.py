44a726695b988bf65c72ab39a3cff9697bf36769
hecyoon <84567556+hecyoon@users.noreply.github.com>
2023-06-25 15:30:48 +0900

    WIP2222

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 CUSTOM_APPS = [
+    "medias.apps.MediasConfig",
     "bookings.apps.BookingsConfig",
     "wishlists.apps.WishlistsConfig",
     "reviews.apps.ReviewsConfig",

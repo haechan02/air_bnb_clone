ba26758e41e08c012d89f7b605c9a843f1ee8f31
hecyoon <84567556+hecyoon@users.noreply.github.com>
2023-06-25 15:16:57 +0900

    WIP

--- backend/bookings/models.py
+++ backend/bookings/models.py
@@ -29,3 +29,19 @@ class Booking(CommonModel):
         blank=True,
         on_delete=models.SET_NULL,
     )
+    check_in = models.DateField(
+        null=True,
+        blank=True,
+    )
+    check_out = models.DateField(
+        null=True,
+        blank=True,
+    )
+    experience_time = models.DateTimeField(
+        null=True,
+        blank=True,
+    )
+    guests = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.kind.title()} booking for: {self.user}"
